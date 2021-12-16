2df139ada8bfeb1007533d48b664c83fffbad43f
Nathan Kuo <nathan.kuo.bcit@gmail.com>
2021-12-16 10:37:59 +0100

    Add status command

---
+++ Commands/Status.js
@@ -0,0 +1,79 @@
+/** @format */
+
+const Command = require("../Structures/Command.js");
+
+const Discord = require("discord.js");
+
+const ProfileModel = require("../Models/profileSchema.js");
+
+const GUILD_NAME = "NoFapGuru's Bot Testing Server";
+
+const CHANNEL_NAME = "daily-streak";
+
+module.exports = new Command({
+	name: "status",
+	description: "Show current user status",
+	permission: "SEND_MESSAGES",
+	async run(message, args, client) {
+
+        //Check if the command is allowed in the channel
+        
+        if (message.channel.name !== 'daily-streak') {
+            message.channel.send(`This command is not allowed in ${message.channel.name}`);
+            return;
+        }
+
+
+        await ProfileModel.
+        find({
+            userId: client.user.id
+        })
+        .exec(function (_err, _profile) {
+            if (_err){
+                return res.status(422).json({
+                    errors: {
+                        name: _err.name,
+                        message: _err.message
+                    },
+                });
+            }
+
+            const guild = client.guilds.cache.find(
+                g => g.name == GUILD_NAME
+            );
+
+            const channel = guild.channels.cache.find(
+                c => c.name == CHANNEL_NAME
+            );
+
+            const embed = new Discord.MessageEmbed();
+            const streak = _profile[0].streak;
+
+            embed
+                .setTitle("Current Status")
+                .setColor("GREEN")
+                .setAuthor(client.user.tag)
+                .setThumbnail(client.user.avatarURL({ dynamic: true }))
+                .addFields(
+                    {
+                        name: "Account Creation Day",
+                        value: client.user.createdAt.toUTCString().toString(),
+                        inline: true
+                    },
+                    {
+                        name: "Current Streak",
+                        value: streak.toString(),
+                        inline: true
+                    }
+                );
+
+            channel.send({ embeds: [embed] });        
+
+        });
+
+
+		
+	}
+});
+
+
